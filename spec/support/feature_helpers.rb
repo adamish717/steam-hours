228fa4ab1a8c6373581c80d800dbc8cef1a62dc6
Sinetheta <k.attfield@gmail.com>
2015-02-08 15:07:13 -0800

    Style navbar and login page

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -3,10 +3,10 @@ module FeatureHelpers
     user ||= FactoryGirl.create(:user)
     visit user_session_path
     within('#new_user') do
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
     end
-    click_button 'Log in'
+    click_button 'Log In'
     expect(page).to have_content 'Signed in successfully'
     user
   end
