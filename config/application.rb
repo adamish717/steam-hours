b888e323583d28302507d200b65ac7c8d22af909
Sinetheta <k.attfield@gmail.com>
2015-02-15 10:29:44 -0800

    Use gmail for mail delivery

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,15 @@ module SteamHours
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :user_name            => ENV['GMAIL_SMTP_LOGIN'],
+      :password             => ENV['GMAIL_SMTP_PASSWORD'],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+    }
   end
 end
