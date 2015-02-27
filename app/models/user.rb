1a7706bb30e9122421d198dedbc23b8895d892ff
Sinetheta <k.attfield@gmail.com>
2015-02-26 15:45:34 -1000

    Add google sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :invitable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :invitable, :omniauthable, :recoverable, :rememberable, :trackable, :validatable, :omniauth_providers => [:google_oauth2]
   has_many :entries
+
+  def self.find_for_google_oauth2(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create name: data['name'],
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+    end
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   resources :clients
 
-  devise_for :users, controllers: { sessions: 'sessions', invitations: 'users/invitations' }
+  devise_for :users, controllers: {
+    invitations: 'users/invitations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'sessions'
+  }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
