1f6d532923f1634e562599e6282ce67b80ad36d0
Sinetheta <k.attfield@gmail.com>
2015-02-28 17:33:38 -1000

    Order Client purchase orders by title

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ActiveRecord::Base
-  has_many :purchase_orders, inverse_of: :client, dependent: :destroy
+  has_many :purchase_orders, -> { order 'title ASC' }, inverse_of: :client, dependent: :destroy
   has_many :entries, through: :purchase_orders
   validates :name, presence: true, uniqueness: true
 end
